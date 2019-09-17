0544964d0ad84e6875c8ae8ea1310b03ff6e5297
Courtney Schild <courtneyschild@gmail.com>
2019-09-17 13:12:06 -0700

    adjust spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     $("#results").show();
+    var elmnt = document.getElementById("results");
+    elmnt.scrollIntoView();
 
     event.preventDefault();
   });
