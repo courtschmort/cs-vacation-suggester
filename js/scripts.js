86982ff8c84270dcb644ff4ae8d94bfff1cf8a8e
Courtney Schild <courtneyschild@gmail.com>
2019-09-17 14:52:35 -0700

    add radio buttons for roundtrip and one-way

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
+
+    let firstNameInput = $("input#firstName").val();
+    let lastNameInput = $("input#lastName").val();
+    $(".firstName").text(firstNameInput);
+    $(".lastName").text(lastNameInput);
+    if(firstNameInput==="") {
+      alert("Your first name is required.")
+    }
+    if(lastNameInput==="") {
+      alert("Your last name is required.")
+    }
+
     $("#results").show();
-    var elmnt = document.getElementById("results");
+    let elmnt = document.getElementById("results");
     elmnt.scrollIntoView();
 
     event.preventDefault();
