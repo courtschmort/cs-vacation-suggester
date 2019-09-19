8c2040c35308ae79d8dc676e6c085cc8840ce256
Courtney Schild <courtneyschild@gmail.com>
2019-09-19 13:39:32 -0700

    add functionality to show and hide results

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
   $(".type").click(function() {
     let type = $("input:radio[name=type]:checked").val();
     if (type === "one-way") {
-        $("#return-date").hide();
-      } else {
-        $("#return-date").show();
-      }
+      $("#return-date").hide();
+    } else {
+      $("#return-date").show();
+    }
   });
 
   $("form").submit(function(event) {
@@ -14,10 +14,18 @@ $(document).ready(function() {
     let lastNameInput = $("input#last-name").val();
     $(".first-name").text(firstNameInput);
     $(".last-name").text(lastNameInput);
-    
 
+    let climateInput = $("#climate").val();
+    let activityInput = $("input:radio[name=activity]:checked").val();
 
     $("#results").show();
+    if (climateInput === "tropical" && activityInput === "explore") {
+      $("#peru").show();
+      $("#bahamas").hide();
+    } else {
+      $("#results").hide();
+    }
+
     let elmnt = document.getElementById("results");
     elmnt.scrollIntoView();
 
